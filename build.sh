26c870f869246c70872f8e26e85b9b1f36cc36e9
shahilmohd <73754576+shahilmohd@users.noreply.github.com>
2023-03-31 12:48:42 +0530

    added some import installations

---
+++ build.sh
@@ -0,0 +1,9 @@
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install numpy
+pip install pandas
+pip install scikit-learn
+pip install seaborn
+pip install mlxtend
